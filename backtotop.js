be7890b72809afe7688ef78a3f02cc12f0eda126
roger87 <willy32136@gmail.com>
2019-07-27 20:09:51 +0800

    移動動畫

--- backtotop.js
+++ backtotop.js
@@ -8,16 +8,28 @@ $(document).ready(function () {
         $("html, body").animate({scrollTop:0},1000);
    }); 
 
-   $("#a1").click(function (e){
+//    $("#a1").click(function (e){
        
-      var top = $("#section1").postion().top;
-      $("html, body").animate({scrollTop:top},500);
-   });
+//       var top = $("#section1").postion().top;
+//       $("html, body").animate({scrollTop:top},500);
+//    });
+//函式會有小括號
 
+//API
+//自訂函式(參數) {內容}
+function scrollToPosition(btn, pos, dur) {
+    
+    $(btn).click(function (e) {
+        var top = $(pos).position().top;
+        $("html, body").animate({scrollTop: top}, dur);
+    });
+}
 
-      var top1 = $("#section2").postion().top();
-      $("html, body").animate({scrollTop:top1},500);
-});
+scrollToPosition("#a1","#section1", 200);
+scrollToPosition("#a2","#section2", 400);
+scrollToPosition("#a3","#section3", 600);
+scrollToPosition("#a4","#section4", 800);
+scrollToPosition("#a5","#section5", 1000);
 // 測距離
 //    alert($(window).scrollTop());
     $("#btnTop").fadeOut(0);    //按鈕隱藏 - 也可以設定在css內
