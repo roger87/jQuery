edbe3be30ee254da7b25885fd3f327b82acab002
roger87 <willy32136@gmail.com>
2019-07-27 15:21:20 +0800

    版面

---
+++ learn.js
@@ -0,0 +1,19 @@
+// $()搜尋物件
+// document 網頁文字
+// . 執行某件事
+// ready() 準備完成
+// function () {} 匿名函式
+
+
+$(document).ready(function () {  
+    alert("我是大帥哥")
+
+    $("h1").hide();
+    $("h2").hide();
+
+    $("#box1").addClass("gold");
+});
+
+function A () {
+
+}
